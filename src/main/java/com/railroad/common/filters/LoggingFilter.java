72dbbc7f2de86476a61de083a8f0a2b9d0c3a1af
Siya Nkosi <ntokozonkosi07@gmail.com>
2020-02-05 00:27:35 +0200

    used the logger producer

--- src/main/java/com/railroad/common/filters/LoggingFilter.java
+++ src/main/java/com/railroad/common/filters/LoggingFilter.java
@@ -1,7 +1,10 @@
 package com.railroad.common.filters;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.ext.Provider;
@@ -12,12 +15,12 @@ import com.railroad.common.annotation.Log;
 @Provider
 public class LoggingFilter implements ContainerRequestFilter {
 
+	@Inject
+	private Logger logger;
+	
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
-		// TODO Auto-generated method stub
-		System.out.println("-----------------------------------------------------------------");
-		System.out.println(requestContext.getRequest().getMethod() +" "+ requestContext.getUriInfo().getAbsolutePath().getPath());
-		System.out.println("-----------------------------------------------------------------");
+		logger.log(Level.INFO, requestContext.getRequest().getMethod() +" "+ requestContext.getUriInfo().getAbsolutePath().getPath());
 	}
 
 }
