474fa94f67686bceafcf6273286e2de12e0d993b
Siya Nkosi <ntokozonkosi07@gmail.com>
2020-02-08 03:48:49 +0200

    scoping the producer

--- src/main/java/com/railroad/common/producers/EntityManagerProducer.java
+++ src/main/java/com/railroad/common/producers/EntityManagerProducer.java
@@ -1,9 +1,11 @@
 package com.railroad.common.producers;
 
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+@RequestScoped
 public class EntityManagerProducer {
     @Produces
     @PersistenceContext
