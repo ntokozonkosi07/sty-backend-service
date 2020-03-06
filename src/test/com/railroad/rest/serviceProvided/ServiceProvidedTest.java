02a5d104dcaa13d9aeb15c32120ad2152a6e8d48
Siya Nkosi <ntokozonkosi07@gmail.com>
2020-03-06 05:05:13 +0200

    adapt from json test file

--- src/test/com/railroad/rest/serviceProvided/ServiceProvidedTest.java
+++ src/test/com/railroad/rest/serviceProvided/ServiceProvidedTest.java
@@ -5,7 +5,10 @@ import com.railroad.common.entityAdapters.EntityAdapter;
 import com.railroad.common.filters.LoggingFilter;
 import com.railroad.common.producers.EntityManagerProducer;
 import com.railroad.configuration.config;
+import com.railroad.entity.Artist;
+import com.railroad.entity.Reservation;
 import com.railroad.entity.User;
+import com.railroad.entity.UserRating;
 import com.railroad.entity.requirement.Requirement;
 import com.railroad.entity.serviceProvided.ServiceProvided;
 import com.railroad.rest.exception.mappers.NoResultExceptionMapper;
@@ -18,6 +21,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.eclipse.yasson.internal.JsonBindingBuilder;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
@@ -28,12 +32,13 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import javax.json.JsonObject;
+import javax.json.*;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
@@ -58,12 +63,95 @@ public class ServiceProvidedTest {
 
         @Override
         public JsonObject adaptToJson(ServiceProvided obj) throws Exception {
-            return null;
+            JsonArrayBuilder jsonReqArr = Json.createArrayBuilder();
+            obj.getRequirements().forEach(r -> {
+                JsonArrayBuilder jsonServArr = Json.createArrayBuilder();
+
+                JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder()
+                        .add("name", r.getName())
+                        .add("price", r.getPrice())
+                        .add("description", r.getDescription())
+                        .add("servicesProvided", jsonServArr);
+
+                if(obj.getId() != null) jsonObjBuilder.add("id", obj.getId());
+
+                JsonObject jsonObj = jsonObjBuilder.build();
+                jsonReqArr.add(jsonObj);
+            });
+
+            JsonArray jsonArtArr = Json.createArrayBuilder().build();
+            obj.getArtists().forEach(a -> {
+                JsonArray jsonArr = Json.createArrayBuilder().build();
+                JsonObjectBuilder jsonObjectBuilder = Json.createObjectBuilder()
+                        .add("email", a.getEmail())
+                        .add("name", a.getName())
+                        .add("lastName", a.getLastName())
+                        .add("picture", "")
+                        .add("reservations",jsonArr)
+                        .add("userRatings",jsonArr)
+                        .add("servicesProvided",jsonArr);
+
+                if(obj.getId() != null) jsonObjectBuilder.add("id", obj.getId());
+                JsonObject jsonObj = jsonObjectBuilder.build();
+
+                jsonArtArr.add(jsonObj);
+            });
+
+            JsonObjectBuilder jsonObjBuilder = Json.createObjectBuilder().
+                    add("name", obj.getName()).
+                    add("price", obj.getPrice()).
+                    add("requirements", jsonReqArr).
+                    add("artists", jsonArtArr);
+
+            if(obj.getId() != null){
+                jsonObjBuilder.add("id", obj.getId());
+            }
+
+            return jsonObjBuilder.build();
         }
 
         @Override
         public ServiceProvided adaptFromJson(JsonObject obj) throws Exception {
-            return null;
+            ServiceProvided serv = new ServiceProvided();
+            serv.setId(((JsonNumber)obj.get("id")).longValue());
+            serv.setName(((JsonString)obj.get("name")).getString());
+            serv.setPrice(((JsonNumber)obj.get("price")).doubleValue());
+
+            Collection<Requirement> reqs = new ArrayList<Requirement>();
+            JsonArray jsonReqs = obj.get("requirements").asJsonArray();
+            if (jsonReqs != null) {
+                jsonReqs.forEach(r ->{
+                    Requirement req = new Requirement();
+                    req.setId(((JsonNumber)((JsonObject)r).get("id")).longValue());
+                    req.setName(((JsonString)((JsonObject)r).get("name")).getString());
+                    req.setDescription(((JsonString)((JsonObject)r).get("description")).getString());
+                    req.setPrice(((JsonNumber)((JsonObject)r).get("price")).doubleValue());
+                    reqs.add(req);
+                });
+            }
+
+            serv.setRequirements(reqs);
+
+            Collection<Artist> artists = new ArrayList<Artist>();
+            JsonArray jsonArts = obj.get("artists").asJsonArray();
+            if(jsonArts !=null){
+                jsonArts.forEach(a -> {
+                    Artist artist = new Artist();
+                    artist.setId(((JsonNumber)((JsonObject)a).get("id")).longValue());
+                    artist.setLastName(((JsonString)((JsonObject)a).get("lastName")).getString());
+                    artist.setName(((JsonString)((JsonObject)a).get("name")).getString());
+                    artist.setEmail(((JsonString)((JsonObject)a).get("email")).getString());
+                    artist.setPicture(null);
+                    artist.setUserRatings(new ArrayList<UserRating>());
+                    artist.setReservation(new ArrayList<Reservation>());
+
+                    artists.add(artist);
+                });
+            }
+
+            serv.setArtists(artists);
+
+            return serv;
         }
     });
 
@@ -99,14 +187,14 @@ public class ServiceProvidedTest {
     public void should_save_service_provided() throws IOException {
         HttpPost post = new HttpPost(url);
 
-        ServiceProvided req = new ServiceProvided();
-        req.setName("Hair Weave");
-        req.setRequirements(new ArrayList<>());
-        req.setArtists(new ArrayList<>());
-        req.setPrice(5.21);
+        ServiceProvided serv = new ServiceProvided();
+        serv.setName("Hair Weave");
+        serv.setRequirements(new ArrayList<>());
+        serv.setArtists(new ArrayList<>());
+        serv.setPrice(5.21);
 
-        Jsonb jsonb = JsonbBuilder.create();
-        String json = jsonb.toJson(req);
+        Jsonb jsonb = JsonbBuilder.create(config);
+        String json = jsonb.toJson(serv);
 
         String result;
 
@@ -123,7 +211,34 @@ public class ServiceProvidedTest {
         }
 
 
-        Requirement _req = jsonb.fromJson(result, Requirement.class);
-        assertEquals(_req.getName(), req.getName());
+        ServiceProvided _serv = jsonb.fromJson(result, ServiceProvided.class);
+        assertEquals(_serv.getName(), serv.getName());
+    }
+
+    @Test
+    @RunAsClient
+    @InSequence(3)
+    public void should_get_list_of_services_provided_after_some_date_has_been_saved() throws IOException {
+        HttpGet request = new HttpGet(url);
+
+        try (
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+                CloseableHttpResponse response = httpClient.execute(request)
+        ) {
+            HttpEntity entity = response.getEntity();
+            String json = null;
+            if (entity != null) {
+                json = EntityUtils.toString(entity);
+                System.out.println(json);
+            }
+
+            Jsonb jsonb = JsonbBuilder.create(config);
+            ArrayList<ServiceProvided> res = jsonb.fromJson(json, ArrayList.class);
+
+            assertEquals(200, response.getStatusLine().getStatusCode());
+            assertEquals(1, res.size());
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        }
     }
 }
