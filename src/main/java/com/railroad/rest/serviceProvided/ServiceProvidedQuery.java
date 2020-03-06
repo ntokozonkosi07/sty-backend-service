b4b8017146893a2495b49cd0fa9287943f69763e
Siya Nkosi <ntokozonkosi07@gmail.com>
2020-03-06 21:52:06 +0200

    get assiciations from db

--- src/main/java/com/railroad/rest/serviceProvided/ServiceProvidedQuery.java
+++ src/main/java/com/railroad/rest/serviceProvided/ServiceProvidedQuery.java
@@ -1,6 +1,8 @@
 package com.railroad.rest.serviceProvided;
 
+import com.railroad.entity.Artist;
 import com.railroad.entity.User;
+import com.railroad.entity.requirement.Requirement;
 import com.railroad.entity.serviceProvided.ServiceProvided;
 import com.railroad.entity.serviceProvided.ServiceProvidedDto;
 
@@ -18,6 +20,27 @@ class ServiceProvidedQuery {
 
     Collection<ServiceProvided> getUsers(Integer maxResults, Integer firstResults){
         return entityManager.createNamedQuery(ServiceProvided.FIND_ALL_SERVICE_PROVIDED, ServiceProvided.class)
+                .setMaxResults(maxResults)
+                .setFirstResult(firstResults)
+                .getResultList();
+    }
+
+    ServiceProvided saveServiceProvided(ServiceProvided serviceProvided) throws EntityExistsException{
+        entityManager.persist(serviceProvided);
+        return serviceProvided;
+    }
+
+    Collection<Requirement> getServiceProvidedRequirements(Long id, Integer maxResults, Integer firstResults) throws IllegalArgumentException{
+        return entityManager.createNamedQuery(ServiceProvided.FIND_ALL_SERVICE_PROVIDED_REQUIREMENTS, Requirement.class)
+                .setParameter("id", id)
+                .setMaxResults(maxResults)
+                .setFirstResult(firstResults)
+                .getResultList();
+    }
+
+    Collection<Artist> getServiceProvidedArtists(Long id, Integer maxResults, Integer firstResults) throws IllegalArgumentException{
+        return entityManager.createNamedQuery(ServiceProvided.FIND_ALL_SERVICE_PROVIDED_ARTISTS, Artist.class)
+                .setParameter("id", id)
                 .setMaxResults(maxResults)
                 .setFirstResult(firstResults)
                 .getResultList();
