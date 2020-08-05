bbc3396d6900c1feb7ec7fd174d17553101ae53c
Siya Nkosi <ntokozonkosi07@gmail.com>
2020-08-05 07:15:40 +0200

    start to use Highlevel elasticsearch client

--- src/main/java/com/railroad/rest/common/AbstractService.java
+++ src/main/java/com/railroad/rest/common/AbstractService.java
@@ -1,33 +1,11 @@
 package com.railroad.rest.common;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.json.bind.Jsonb;
-import javax.json.bind.JsonbBuilder;
-import java.io.IOException;
 import java.util.Optional;
 
 public abstract class AbstractService {
-    private RestHighLevelClient client;
     private Jsonb jsonb;
 
-    @PostConstruct
-    private void init(){
-        client = new RestHighLevelClient(RestClient.builder(
-                new HttpHost("localhost",9200, "http"),
-                new HttpHost("localhost",9300, "http")
-        ));
-        jsonb = JsonbBuilder.create();
-    }
-
-    @PreDestroy
-    private void destroy() throws IOException {
-        client.close();
-    }
 
     protected Integer parameterValidation(Integer maxResults, Optional<Integer> firstResults) throws IllegalArgumentException{
         Integer firstRes = firstResults.isPresent() ? firstResults.get() : 0;
